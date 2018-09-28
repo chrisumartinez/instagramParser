74961f7fdcba52d971cbaea91efce06eaa2f3ff3
Chris Martinez <chrismartinez@Chriss-MacBook-Pro.local>
2018-09-28 08:12:32 -0700

    boiler code

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Chris Martinez on 9/28/18.
+//  Copyright © 2018 Chris Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
